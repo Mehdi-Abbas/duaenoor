caf88b8c03ec0c5950ac99bbcee41245372fa86a
Mehdi Abbas <mehdiabbas799@gmail.com>
2021-10-10 00:32:02 +0500

    first commit

---
+++ read.js
@@ -0,0 +1,1 @@
+document.getElementById("menu").style.height=screen.height+"px";
---
+++ readlisten.js
@@ -0,0 +1,5 @@
+document.getElementById("menu").style.height=screen.height+"px";
+
+
+var audio = new Audio('Dua-e-Noor.mp3');
+audio.play();
